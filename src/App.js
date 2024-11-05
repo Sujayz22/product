6197489dbe29a7da921bc503c2b29f44ac98ca27
Sujay Panda <youaremysoniye@gmail.com>
2024-11-05 23:33:38 +0530

    added basename to fix blank page on deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/product'>
       <Routes>
          <Route path="/" element={ <Home />}/>
       </Routes>
